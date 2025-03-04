cfbf9ca0b71ee1cc7d056c71841b4e3c8096389a
Riyas <m.riyas.muhammed@gmail.com>
2025-03-04 16:51:01 +0530

    complted map

--- src/components/contact/Map.jsx
+++ src/components/contact/Map.jsx
@@ -1,20 +1,24 @@
+import "./contact.css";
+import "leaflet/dist/leaflet.css";//style file for map from leaflet
 
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Map = () => {
+
+  const position = [12.87818, 77.76228]
+
   return (
-    <div>
-      {/* <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false} className="map">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={position}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            4key Hunger Hackers. <br /> Confident Oxygen mall,<br />Bangalore,Dommasandra.
           </Popup>
         </Marker>
-      </MapContainer> */} map
-    </div>
+      </MapContainer>
   );
 };
 
