b7440e3003fb7f3b37052771383c2d008a49332a
Riyas <m.riyas.muhammed@gmail.com>
2025-03-08 13:56:38 +0530

    finished version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import Hero from "./components/hero/Hero";
-import Menu from "./components/menu/Menu";
-import Contact from "./components/contact/Contact";
+import { lazy, Suspense } from "react";
+import LazyLoad from "react-lazyload";
+// lazy loading for dynamic loading to get faster we pages
+
+const Hero = lazy(() => import("./components/hero/Hero"));
+const Menu = lazy(() => import("./components/menu/Menu"));
+const Contact = lazy(() => import("./components/contact/Contact"));
 
 function App() {
   return (
     <div className="container">
-      <section id="home">
-        <Hero />
-      </section>
-        <Menu />
-      <section id="contact">
-        <Contact />
-      </section>
-
+      <Suspense fallback="Loading...">
+        <LazyLoad height={"100vh"}>
+          <section id="home">
+            <Hero />
+          </section>
+        </LazyLoad>
+      </Suspense>
+      <Suspense fallback="Loading...">
+        <LazyLoad height={"700vh"} offset={-100}>
+          <Menu />
+        </LazyLoad>
+      </Suspense>
+      <Suspense fallback="Loading...">
+        <LazyLoad height={"100vh"} offset={-100}>
+          <section id="contact">
+            <Contact />
+          </section>
+        </LazyLoad>
+      </Suspense>
     </div>
   );
 }
